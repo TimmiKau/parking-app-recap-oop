78c5ea6e791ff461294f50b1cebfcc13c9309cf7
Nalbi Stas <stas.nalbi@gmail.com>
2025-11-18 13:40:13 +0100

    Added ReservationDao interface

--- src/main/java/se/lexicon/dao/ReservationDao.java
+++ src/main/java/se/lexicon/dao/ReservationDao.java
@@ -2,12 +2,15 @@ package se.lexicon.dao;
 
 import se.lexicon.model.Reservation;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface ReservationDao {
     Reservation create(Reservation reservation);
 
     Optional<Reservation> findById(String reservationId);
+
+    List<Reservation> findAll();
 
     void update(Reservation reservation);
 }
