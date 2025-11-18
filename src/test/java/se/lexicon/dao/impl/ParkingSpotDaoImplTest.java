dd00aa511904a9ba5ec4502aeb0941cdc8c0df3b
Spindelen <erik@manboy.se>
2025-11-18 22:36:14 +0100

    Done but with errors.

---
+++ src/test/java/se/lexicon/dao/impl/ParkingSpotDaoImplTest.java
@@ -0,0 +1,27 @@
+package se.lexicon.dao.impl;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class ParkingSpotDaoImplTest {
+
+    @Test
+    void create() {
+    }
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void findBySpotNumber() {
+    }
+
+    @Test
+    void findAvailableSpots() {
+    }
+
+    @Test
+    void update() {
+    }
+}
