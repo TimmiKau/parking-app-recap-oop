552c51fae3199ecd43e60650044ed962db5e9560
TimmiKau <timmi.karlsson@hotmail.se>
2025-11-19 10:15:13 +0100

    Fix add parkingspots method

--- src/main/java/se/lexicon/Main.java
+++ src/main/java/se/lexicon/Main.java
@@ -7,8 +7,6 @@ import se.lexicon.model.ParkingSpot;
 public class Main {
     public static void main(String[] args) {
 
-        ParkingSpot parkingSpot1 = new ParkingSpot(1,2,false);
-
         ParkingApp parkingApp = new ParkingApp();
         parkingApp.start();
 
